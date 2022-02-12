ee72f28cbec9be2d02598b59972365a7560744c0
tag_m <you@example.com>
2022-02-12 23:35:59 +0100

    LL Script fixed

--- loaded_lion/loaded_lion.js
+++ loaded_lion/loaded_lion.js
@@ -16,76 +16,6 @@ if (isDarkTheme) {
 }
 widget.setPadding(padding, padding, padding, padding);
 
-widget.url = 'https://www.coingecko.com/en/coins/crypto-com-coin';
-
-const headerStack = widget.addStack();
-headerStack.setPadding(0, 0, 25, 0);
-
-
-async function getCollection(id) {
-  const payload = {
-    "operationName": "GetCollection",
-    "variables": {
-        "collectionId": id
-    },
-    "query": "query GetCollection($collectionId: ID!) {\n  public {\n    collection(id: $collectionId) {\n      id\n      name\n      description\n      categories\n      banner {\n        url\n        __typename\n      }\n      logo {\n        url\n        __typename\n      }\n      creator {\n        displayName\n        __typename\n      }\n      aggregatedAttributes {\n        label: traitType\n        options: attributes {\n          value: id\n          label: value\n          total\n          __typename\n        }\n        __typename\n      }\n      metrics {\n        items\n        minAuctionListingPriceDecimal\n        minSaleListingPriceDecimal\n        owners\n        totalSalesDecimal\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}\n"
-  };
-
-  let req = new Request("https://crypto.com/nft-api/graphql");
-  req.method = "post";
-  req.headers = {
-  //  "x-something": "foo bar",
-   "Content-Type": "application/json"
-  };
-  req.body = JSON.stringify(payload);
-  // use loadJSON because the API answers with JSON
-  let res = await req.loadJSON();
-  log(JSON.stringify(res['data']['public']['collection']['name'], null, 2));
-  return res['data']['public']['collection']
-}
-
-function getName(collection) {
-  result = JSON.stringify(collection['name'], null, 2)
-  log(result);
-  return result
-
-}
-
-function getGIF(collection) {
-  result = JSON.stringify(collection['logo']['url'], null, 2)
-  log(result);
-  return result
-}
-
-
-function getFloorPrice(collection) {
-  result = JSON.stringify(collection['metrics']['minSaleListingPriceDecimal'], null, 2)
-  log(result);
-  return result
-}
-
-
-async function buildWidget() {
-    const ll_id = "82421cf8e15df0edcaa200af752a344f"
-    const collection = await getCollection(ll_id)
-    const name = getName(collection)
-    const gifURL = await getGIF(collection)
-    const image = await loadImage('https://d2vi0z68k5oxnr.cloudfront.net/cf737f64-3a38-43a9-98f5-b6d0a12b5e67/original.gif')
-    const floorPrice = await Math.round(getFloorPrice(collection).toString() * 1000) / 1000;;
-  
-    addCrypto(image, name, floorPrice);
-
-const params = args.widgetParameter ? args.widgetParameter.split(",") : [];
-
-const isDarkTheme = params?.[0] === 'dark';
-const padding = 2;
-
-const widget = new ListWidget();
-if (isDarkTheme) {
- widget.backgroundColor = new Color('#1C1C1E');; 
-}
-widget.setPadding(padding, padding, padding, padding);
-
 const headerStack = widget.addStack();
 headerStack.setPadding(0, 0, 25, 0);
 
@@ -142,46 +72,6 @@ async function buildWidget() {
     const floorPrice = getFloorPrice(collection)
   
     addCrypto(image, name, `$${floorPrice}`);
-}
-
-function addCrypto(image, symbol, price) {
-   const rowStack = widget.addStack();
-   rowStack.setPadding(0, 0, 20, 0);
-   rowStack.layoutHorizontally();
-  
-   const imageStack = rowStack.addStack(); 
-   const symbolStack = rowStack.addStack(); 
-   const priceStack = rowStack.addStack(); 
-  
-   imageStack.setPadding(0, 0, 0, 10);
-   symbolStack.setPadding(0, 0, 0, 8);
-  
-   const imageNode = imageStack.addImage(image);
-   imageNode.imageSize = new Size(20, 20);
-   imageNode.leftAlignImage();
-  
-   const symbolText = symbolStack.addText(symbol);
-   symbolText.font = Font.mediumSystemFont(16);
-  
-   const priceText = priceStack.addText(price);
-   priceText.font = Font.mediumSystemFont(16);
-  
-  if (isDarkTheme) {
-    symbolText.textColor = new Color('#FFFFFF');
-  }
-}
-
-
-async function loadImage(imgUrl) {
-    const req = new Request(imgUrl)
-    return await req.loadImage()
-}
-
-await buildWidget();
-
-Script.setWidget(widget);
-Script.complete();
-widget.presentSmall();
 }
 
 function addCrypto(image, symbol, price) {
